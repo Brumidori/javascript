232a7fec18926e024b2513d3d8300c852dc8b7d3
bru_midori <brunamidoriyy@yahoo.com.br>
2022-12-06 14:48:50 -0300

    usando biblioteca moment (formatador horas e dias)

--- primeiro_projeto/app.mjs
+++ primeiro_projeto/app.mjs
@@ -1,3 +1,11 @@
  /*config do projeto - instalacao npm highlight.js, express, webpack,yarn
-  "name": "primeiro_projeto",*/
+*/
 //instalacao npm moment(trabalha com horas)
+
+import moment from 'moment';
+
+moment.locale("pt-BR")
+let data = moment().format('D [de] MMMM [de] YYYY' )
+let hora = moment().format('H:mm:ss')
+console.log(data)
+console.log(hora)

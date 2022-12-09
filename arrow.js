cb4d6b1f4c9cc06b2ab933a1f47c54933588b507
bru_midori <brunamidoriyy@yahoo.com.br>
2022-12-09 17:16:47 -0300

    refatorando callback function

--- arrow.js
+++ arrow.js
@@ -10,6 +10,7 @@ let pedido3 = {
     'nome': 'pedido3',
     'tempo': 2000
 }
+
 const cozinha = (pedido, callback) =>
     setTimeout(() => {
         console.log(pedido['nome'] + '  pronto')
@@ -22,27 +23,32 @@ const garcon = (tarefa, callback) => {
     callback()
 }
 
+const encerrarPedidos = () => {
+    console.log("encerra pedidos")
+    console.log("paga conta")
+}
+
+const fazerPedido1 = garcon("pedido1 para cozinha", () =>
+    cozinha(pedido1, retornarPedido1
+    )
+)
+const retornarPedido1 = (retorno1) => garcon(retorno1, fazerPedido2)
+const fazerPedido2 = () =>
+    garcon("pedido 2 para cozinha", () =>
+        cozinha(pedido2, retornarPedido2)
+    )
+const retornarPedido2 = (retorno2) =>
+    garcon(retorno2, () =>
+        garcon("pedido 3 para a cozinha", fazerPedido3)
+    )
+const fazerPedido3 = () => cozinha(pedido3, retornarPedido3)
+const retornarPedido3 = (retorno3) =>
+    garcon(retorno3, encerrarPedidos())
+
 const janta = () => {
     console.log('iniciando pedidos')
-    garcon("pedido1 para cozinha", () => {
-        cozinha(pedido1, (retorno1) =>
-            garcon(retorno1, () =>
-                garcon("pedido 2 para cozinha", () =>
-                    cozinha(pedido2, (retorno2) =>
-                        garcon(retorno2, () =>
-                            garcon("pedido 3 para a cozinha", () =>
-                                cozinha(pedido3, (retorno3) =>
-                                    garcon(retorno3, () => {
-                                        console.log("encerra pedidos")
-                                        console.log("paga contas")
-                                    })
-                                )
-                            )   
-                        )
-                    )
-                )
-            )
-        )
-    })
+    fazerPedido1()
+
 }
+
 janta()

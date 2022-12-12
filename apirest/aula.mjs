5452a86c5fd5563d226583fcd6593dd54ed04b56
bru_midori <brunamidoriyy@yahoo.com.br>
2022-12-12 16:47:45 -0300

    metodos http com fetch

--- apirest/aula.mjs
+++ apirest/aula.mjs
@@ -4,5 +4,21 @@ fetch("https://jsonplaceholder.typicode.com/posts/1")
 .then((resposta) => resposta.json())
 .then((jsonObject) => console.log(jsonObject))
 
-//com o fetch pegamos um objeto
+//com o fetch pegamos um objeto (resposta)
 //no caso transformamos em json, mas pode transformar em img por ex
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+    "method" : "post",
+    "body" : JSON.stringify({
+        "userID" : 123456,
+        "title" : "publicacao de teste",
+        "body" : "lorem ipsum dolor sit amet",
+    }),
+    "headers" : {
+        "Content-type" : "application/json",
+    }
+})
+.then((resposta) => resposta.json())
+.then((jsonObject) => console.log(jsonObject))
+
+// requisacao - method post  (precisa de body)

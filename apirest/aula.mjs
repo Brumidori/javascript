739e6d491f8200a8783e5fecf16d20baa336506f
bru_midori <brunamidoriyy@yahoo.com.br>
2022-12-12 17:10:31 -0300

    requisitos RESTfull

--- apirest/aula.mjs
+++ apirest/aula.mjs
@@ -21,4 +21,14 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
 .then((resposta) => resposta.json())
 .then((jsonObject) => console.log(jsonObject))
 
-// requisacao - method post  (precisa de body)
+// requisacao - method post/put/patch  (precisa de body)
+// delete e get precisa de ID
+
+/* API REST(full)
+- protocolo cliente/servidor sem estado
+- conjunto de operacoes - POST, GET, PUT, DELETE
+- recursos e URI (identificador unico de recurso)
+- separacao de capas (requisicao/resposta sem saber pra onde vao - so precisa saber o endpoint
+        simplifica a arquitetura em rede - precisa proteger [https])
+- hipermídia (a partir de uma solicitacao posso ter acesso a outras solitacoes)
+*/
